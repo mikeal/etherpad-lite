5980a0bfe673fe3b45d00c9778824c3ab1ab622a
Mikeal Rogers <mikeal.rogers@gmail.com>
2011-09-12 16:37:01 -0700

    Better tracking of database calls.

--- node/server.js
+++ node/server.js
@@ -1,6 +1,6 @@
 /**
- * This module is started with bin/run.sh. It sets up a Express HTTP and a Socket.IO Server. 
- * Static file Requests are answered directly from this module, Socket.IO messages are passed 
+ * This module is started with bin/run.sh. It sets up a Express HTTP and a Socket.IO Server.
+ * Static file Requests are answered directly from this module, Socket.IO messages are passed
  * to MessageHandler and minfied requests are passed to minified.
  */
 
@@ -50,7 +50,7 @@ try
   version = version.substring(0, 7);
   console.log("Your Etherpad Lite git version is " + version);
 }
-catch(e) 
+catch(e)
 {
   console.warn("Can't get git version for server header\n" + e.message)
 }
@@ -77,27 +77,31 @@ async.waterfall([
           , setTimer = metrics.timer('db.set')
           , removeTimer = metrics.timer('db.remove')
           , bulkTimer = metrics.timer('db.bulk')
-          , dbHistogram = metrics.histogram('db.ops')
+          , initHist = metrics.histogram('db.ops.init')
+          , getHist = metrics.histogram('db.ops.get')
+          , setHist = metrics.histogram('db.ops.set')
+          , removeHist = metrics.histogram('db.ops.remove')
+          , bulkHist = metrics.histogram('db.ops.bulk')
           ;
         db.db.db.wrappedDB.on('metric.init', function (duration) {
           initTimer.update(duration)
-          dbHistogram.update('init', (new Date()).getTime())
+          initHist.update(1, (new Date()).getTime())
         })
         db.db.db.wrappedDB.on('metric.get', function (duration) {
           getTimer.update(duration)
-          dbHistogram.update('get', (new Date()).getTime())
+          getHist.update(1, (new Date()).getTime())
         })
         db.db.db.wrappedDB.on('metric.set', function (duration) {
           setTimer.update(duration)
-          dbHistogram.update('set', (new Date()).getTime())
+          setHist.update(1, (new Date()).getTime())
         })
         db.db.db.wrappedDB.on('metric.remove', function (duration) {
           removeTimer.update(duration)
-          dbHistogram.update('remove', (new Date()).getTime())
+          removeHist.update(1, (new Date()).getTime())
         })
         db.db.db.wrappedDB.on('metric.bulk', function (duration) {
           bulkTimer.update(duration)
-          dbHistogram.update('bulk', (new Date()).getTime())
+          bulkHist.update(1, (new Date()).getTime())
         })
       }
       callback.apply(this, arguments)
@@ -108,7 +112,7 @@ async.waterfall([
   {
     //create server
     var app = express.createServer();
-    
+
     //load modules that needs a initalized db
     readOnlyManager = require("./db/ReadOnlyManager");
     exporthtml = require("./utils/ExportHtml");
@@ -118,30 +122,30 @@ async.waterfall([
     padManager = require('./db/PadManager');
     securityManager = require('./db/SecurityManager');
     socketIORouter = require("./handler/SocketIORouter");
-    
-    //install logging      
+
+    //install logging
     var httpLogger = log4js.getLogger("http");
-    app.configure(function() 
+    app.configure(function()
     {
       app.use(log4js.connectLogger(httpLogger, { level: log4js.levels.INFO, format: ':status, :method :url'}));
       app.use(express.cookieParser());
     });
-    
+
     //serve static files
     app.get('/static/*', function(req, res)
-    { 
+    {
       res.header("Server", serverName);
       var filePath = path.join(__dirname, "..", req.url.split("?")[0].slice(1));
       res.sendfile(filePath, { maxAge: exports.maxAge });
     });
-    
+
     //serve minified files
     app.get('/minified/:id', function(req, res, next)
-    { 
+    {
       res.header("Server", serverName);
-      
+
       var id = req.params.id;
-      
+
       if(id == "pad.js" || id == "timeslider.js")
       {
         minify.minifyJS(req,res,id);
@@ -151,14 +155,14 @@ async.waterfall([
         next();
       }
     });
-    
+
     //checks for padAccess
     function hasPadAccess(req, res, callback)
     {
       securityManager.checkAccess(req.params.pad, req.cookies.sessionid, req.cookies.token, req.cookies.password, function(err, accessObj)
       {
         if(err) throw err;
-        
+
         //there is access, continue
         if(accessObj.accessStatus == "grant")
         {
@@ -171,16 +175,16 @@ async.waterfall([
         }
       });
     }
-    
+
     //serve read only pad
     app.get('/ro/:id', function(req, res)
-    { 
+    {
       res.header("Server", serverName);
-      
+
       var html;
       var padId;
       var pad;
-      
+
       async.series([
         //translate the read only pad to a padId
         function(callback)
@@ -188,10 +192,10 @@ async.waterfall([
           readOnlyManager.getPadId(req.params.id, function(err, _padId)
           {
             padId = _padId;
-            
-            //we need that to tell hasPadAcess about the pad  
-            req.params.pad = padId; 
-            
+
+            //we need that to tell hasPadAcess about the pad
+            req.params.pad = padId;
+
             callback(err);
           });
         },
@@ -204,7 +208,7 @@ async.waterfall([
             callback("notfound");
             return;
           }
-          
+
           hasPadAccess(req, res, function()
           {
             //render the html document
@@ -220,29 +224,29 @@ async.waterfall([
         //throw any unexpected error
         if(err && err != "notfound")
           throw err;
-          
+
         if(err == "notfound")
           res.send('404 - Not Found', 404);
         else
           res.send(html);
       });
     });
-        
+
     //serve pad.html under /p
     app.get('/p/:pad', function(req, res, next)
-    {    
+    {
       //ensure the padname is valid and the url doesn't end with a /
       if(!padManager.isValidPadId(req.params.pad) || /\/$/.test(req.url))
       {
         res.send('Such a padname is forbidden', 404);
         return;
       }
-      
+
       res.header("Server", serverName);
       var filePath = path.normalize(__dirname + "/../static/pad.html");
       res.sendfile(filePath, { maxAge: exports.maxAge });
     });
-    
+
     //serve timeslider.html under /p/$padname/timeslider
     app.get('/p/:pad/timeslider', function(req, res, next)
     {
@@ -252,12 +256,12 @@ async.waterfall([
         res.send('Such a padname is forbidden', 404);
         return;
       }
-      
+
       res.header("Server", serverName);
       var filePath = path.normalize(__dirname + "/../static/timeslider.html");
       res.sendfile(filePath, { maxAge: exports.maxAge });
     });
-    
+
     //serve timeslider.html under /p/$padname/timeslider
     app.get('/p/:pad/export/:type', function(req, res, next)
     {
@@ -267,7 +271,7 @@ async.waterfall([
         res.send('Such a padname is forbidden', 404);
         return;
       }
-    
+
       var types = ["pdf", "doc", "txt", "html", "odt"];
       //send a 404 if we don't support this filetype
       if(types.indexOf(req.params.type) == -1)
@@ -275,23 +279,23 @@ async.waterfall([
         next();
         return;
       }
-      
+
       //if abiword is disabled, and this is a format we only support with abiword, output a message
       if(settings.abiword == null && req.params.type != "html" && req.params.type != "txt" )
       {
         res.send("Abiword is not enabled at this Etherpad Lite instance. Set the path to Abiword in settings.json to enable this feature");
         return;
       }
-      
+
       res.header("Access-Control-Allow-Origin", "*");
       res.header("Server", serverName);
-      
+
       hasPadAccess(req, res, function()
       {
         exportHandler.doExport(req, res, req.params.pad, req.params.type);
       });
     });
-    
+
     //handle import requests
     app.post('/p/:pad/import', function(req, res, next)
     {
@@ -301,70 +305,70 @@ async.waterfall([
         res.send('Such a padname is forbidden', 404);
         return;
       }
-    
+
       //if abiword is disabled, skip handling this request
       if(settings.abiword == null)
       {
         next();
-        return; 
+        return;
       }
-      
+
       res.header("Server", serverName);
-      
+
       hasPadAccess(req, res, function()
       {
         importHandler.doImport(req, res, req.params.pad);
       });
     });
-    
+
     var apiLogger = log4js.getLogger("API");
-    
+
     //This is a api call, collect all post informations and pass it to the apiHandler
     app.get('/api/1/:func', function(req, res)
     {
       res.header("Server", serverName);
       res.header("Content-Type", "application/json; charset=utf-8");
-    
+
       apiLogger.info("REQUEST, " + req.params.func + ", " + JSON.stringify(req.query));
-      
+
       //wrap the send function so we can log the response
       res._send = res.send;
       res.send = function(response)
       {
         response = JSON.stringify(response);
         apiLogger.info("RESPONSE, " + req.params.func + ", " + response);
-        
+
         //is this a jsonp call, if yes, add the function call
         if(req.query.jsonp)
           response = req.query.jsonp + "(" + response + ")";
-        
+
         res._send(response);
       }
-      
+
       //call the api handler
       apiHandler.handle(req.params.func, req.query, req, res);
     });
-    
+
     //The Etherpad client side sends information about how a disconnect happen
     app.post('/ep/pad/connection-diagnostic-info', function(req, res)
     {
-      new formidable.IncomingForm().parse(req, function(err, fields, files) 
-      { 
+      new formidable.IncomingForm().parse(req, function(err, fields, files)
+      {
         console.log("DIAGNOSTIC-INFO: " + fields.diagnosticInfo);
         res.end("OK");
       });
     });
-    
+
     //The Etherpad client side sends information about client side javscript errors
     app.post('/jserror', function(req, res)
     {
-      new formidable.IncomingForm().parse(req, function(err, fields, files) 
-      { 
+      new formidable.IncomingForm().parse(req, function(err, fields, files)
+      {
         console.error("CLIENT SIDE JAVASCRIPT ERROR: " + fields.errorInfo);
         res.end("OK");
       });
     });
-    
+
     //serve index.html under /
     app.get('/', function(req, res)
     {
@@ -372,7 +376,7 @@ async.waterfall([
       var filePath = path.normalize(__dirname + "/../static/index.html");
       res.sendfile(filePath, { maxAge: exports.maxAge });
     });
-    
+
     //serve robots.txt
     app.get('/robots.txt', function(req, res)
     {
@@ -380,7 +384,7 @@ async.waterfall([
       var filePath = path.normalize(__dirname + "/../static/robots.txt");
       res.sendfile(filePath, { maxAge: exports.maxAge });
     });
-    
+
     //serve favicon.ico
     app.get('/favicon.ico', function(req, res)
     {
@@ -396,7 +400,7 @@ async.waterfall([
         }
       });
     });
-    
+
     //let the server listen
     app.listen(settings.port, settings.ip);
     console.log("Server is listening at " + settings.ip + ":" + settings.port);
@@ -407,7 +411,7 @@ async.waterfall([
     {
       // stop accepting connections NOW!
       app.pause()
-      
+
       if(err && err.stack)
       {
         console.error(err.stack);
@@ -416,20 +420,20 @@ async.waterfall([
       {
         console.error(err);
       }
-      
+
       // If we already tried to run shutdown and get another signal hard kill our pid
       if (shutdownCalled) {
         console.error('hard kill')
         return process.kill(process.pid, 'SIGKILL')
       }
       shutdownCalled = true;
-      
+
       //ensure there is only one graceful shutdown running
       if(onShutdown) return;
       onShutdown = true;
-    
+
       console.log("graceful shutdown...");
-      
+
       //stop the http server
       app.close();
 
@@ -444,7 +448,7 @@ async.waterfall([
         console.log('no db shutdown.')
         process.exit();
       }
-      
+
       setTimeout(function(){
         console.error('shutdown timeout')
         process.exit(1);
@@ -458,22 +462,22 @@ async.waterfall([
       //https://github.com/joyent/node/issues/1553
       process.on('SIGINT', gracefulShutdown);
     }
-    
+
     process.on('uncaughtException', gracefulShutdown);
 
     //init socket.io and redirect all requests to the MessageHandler
     var io = socketio.listen(app);
-    
+
     //this is only a workaround to ensure it works with all browers behind a proxy
     //we should remove this when the new socket.io version is more stable
     io.set('transports', ['xhr-polling']);
-    
+
     var socketIOLogger = log4js.getLogger("socket.io");
     io.set('logger', {
       debug: function (str)
       {
         socketIOLogger.debug(str);
-      }, 
+      },
       info: function (str)
       {
         socketIOLogger.info(str);
@@ -487,20 +491,20 @@ async.waterfall([
         socketIOLogger.error(str);
       },
     });
-    
-        
+
+
     //minify socket.io javascript
     if(settings.minify)
       io.enable('browser client minification');
-    
+
     var padMessageHandler = require("./handler/PadMessageHandler");
     var timesliderMessageHandler = require("./handler/TimesliderMessageHandler");
-    
+
     //Initalize the Socket.IO Router
     socketIORouter.setSocketIO(io);
     socketIORouter.addComponent("pad", padMessageHandler);
     socketIORouter.addComponent("timeslider", timesliderMessageHandler);
-        
-    callback(null);  
+
+    callback(null);
   }
 ]);
